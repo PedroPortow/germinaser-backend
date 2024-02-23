bd67152efb5d17c537447afd63f8e900eb0beafb
PedroPortow <pedrolportow@gmail.com>
2024-02-23 18:49:21 -0300

    feat: added the room and booking model

---
+++ app/controllers/rooms_controller.rb
@@ -0,0 +1,50 @@
+class RoomsController < ApplicationController
+  before_action :set_room, only: [:show, :update, :destroy]
+
+  # GET /rooms
+  def index
+    @rooms = Room.all
+    render json: @rooms
+  end
+
+  # GET /rooms/:id
+  def show
+    render json: @room
+  end
+
+  # POST /rooms
+  def create
+    @room = Room.new(room_params)
+    if @room.save
+      render json: @room, status: :created, location: @room
+    else
+      render json: @room.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /rooms/:id
+  def update
+    if @room.update(room_params)
+      render json: @room
+    else
+      render json: @room.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /rooms/:id
+  def destroy
+    @room.destroy
+    head :no_content
+  end
+
+  private
+    def set_room
+      @room = Room.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+      render json: { error: "Room not found" }, status: :not_found
+    end
+
+    def room_params
+      params.require(:room).permit(:name, :clinic_id)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'rooms/index'
+  get 'rooms/show'
+  get 'rooms/new'
+  get 'rooms/edit'
+  get 'rooms/create'
+  get 'rooms/update'
+  get 'rooms/destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
