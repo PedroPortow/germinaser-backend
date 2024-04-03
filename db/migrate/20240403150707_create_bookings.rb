4f1dbb522d39ef569593d4a46fad522d276e8e7a
PedroPortow <pedrolportow@gmail.com>
2024-04-03 12:08:14 -0300

    booking model

---
+++ db/migrate/20240403150707_create_bookings.rb
@@ -0,0 +1,12 @@
+class CreateBookings < ActiveRecord::Migration[7.0]
+  def change
+    create_table :bookings do |t|
+      t.references :room, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
+      t.datetime :start_time
+      t.datetime :canceled_at
+
+      t.timestamps
+    end
+  end
+end
