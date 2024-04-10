d3311a2423fe3fff548654e0c30275837b3e1a79
PedroPortow <pedrolportow@gmail.com>
2024-04-09 22:39:37 -0300

    feat: added user bookings count to user serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,7 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :email, :credits, :name, :role
+  attributes :id, :email, :credits, :name, :role, :bookings_count 
+
+  def bookings_count
+    object.bookings.count
+  end 
 end
