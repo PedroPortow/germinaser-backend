6a39b0951e8f1ee0f7f1f513c5e5173d6e9794fd
PedroPortow <pedrolportow@gmail.com>
2024-04-10 00:12:01 -0300

    feat: user serializer using bookings scopes to show user bookings data

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,7 +1,21 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :email, :credits, :name, :role, :bookings_count 
+  attributes :id, :email, :credits, :name, :role,
+             :active_bookings_count, :done_bookings_count, 
+             :canceled_bookings_count, :total_bookings_count
 
-  def bookings_count
+  def active_bookings_count
+    object.bookings.active.count
+  end
+
+  def done_bookings_count
+    object.bookings.done.count
+  end
+
+  def canceled_bookings_count
+    object.bookings.canceled.count
+  end
+
+  def total_bookings_count
     object.bookings.count
-  end 
+  end
 end
