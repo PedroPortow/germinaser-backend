72d7c178ca5d6bb3d6fe0867ebaaad925fd54906
PedroPortow <pedrolportow@gmail.com>
2024-04-12 08:33:00 -0300

    fix: fixed dumbass validation again

--- app/models/room.rb
+++ app/models/room.rb
@@ -2,6 +2,6 @@ class Room < ApplicationRecord
   belongs_to :clinic
   has_many :bookings
 
-  validates :clinic, presence: true, uniqueness: true
+  validates :clinic, presence: true
   validates :name, presence: true, uniqueness: { scope: :clinic_id, message: "O nome da sala deve ser único dentro de cada clínica" }
 end
