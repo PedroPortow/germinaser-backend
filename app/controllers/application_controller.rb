80890bb1227feec68e4dfbee28b30d41b2087772
PedroPortow <pedrolportow@gmail.com>
2024-04-12 08:34:28 -0300

    fix: fixed helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::API
   def current_user_admin_or_owner
-    current_user.admin? || current
+    current_user.admin? || current_user.owner?
   end 
 end
