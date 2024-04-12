65e8d0e08be8d4a3e1488dff37fe574437c91918
PedroPortow <pedrolportow@gmail.com>
2024-04-12 10:16:00 -0300

    fix: removing user bookings when deleting user

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
   enum role: [:user, :admin, :owner]
   after_initialize :set_default_role, if: :new_record?
   after_initialize :set_default_credits, if: :new_record?
-  has_many :bookings
+  has_many :bookings, dependent: :destroy
 
 
   def self.roles_keys
