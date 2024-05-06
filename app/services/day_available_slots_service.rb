4e56044023d8f0678894ceca70ebb9c461deebcb
porto <pedrolportow@gmail.com>
2024-05-05 23:30:53 -0300

    fix: fixed application nwo storing date in UTC and service handling convertion to pt-br

--- app/services/day_available_slots_service.rb
+++ app/services/day_available_slots_service.rb
@@ -1,12 +1,15 @@
 class DayAvailableSlotsService
   def initialize(room_id, date)
     @room = Room.find(room_id)
-    @date = date
+    @date = date.in_time_zone('Brasilia')
   end
 
   def call
+    Time.zone = 'Brasilia'
+
     start_period = @date.beginning_of_day + 8.hours
     end_period = @date.beginning_of_day + 21.hours
+
     slots = (start_period.to_i..end_period.to_i).step(1.hour).map { |t| Time.zone.at(t) }
 
     current_time = Time.zone.now
@@ -18,7 +21,7 @@ class DayAvailableSlotsService
     existing_bookings = Booking.where(room_id: @room.id, start_time: start_period...end_period)
 
     slots.reject! do |slot|
-      existing_bookings.any? { |booking| booking.start_time.strftime('%H:%M') == slot.strftime('%H:%M') }
+      existing_bookings.any? { |booking| booking.start_time.in_time_zone('Brasilia').strftime('%H:%M') == slot.strftime('%H:%M') && booking.canceled_at.nil? }
     end
 
     formatted_slots = slots.map { |slot| slot.strftime('%H:%M') }
