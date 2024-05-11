1af79a162df7dca6ee558eb2fd835cf1c2a4ac1b
porto <pedrolportow@gmail.com>
2024-05-11 14:56:26 -0300

    feat: active bookings count route

--- app/controllers/current_user_controller.rb
+++ app/controllers/current_user_controller.rb
@@ -1,11 +1,16 @@
 class CurrentUserController < ApplicationController
   before_action :authenticate_user!
-  
+
   def index
     render json: current_user, status: :ok
   end
 
   def available_credits
     render json: { credits: current_user.credits }, status: :ok
+  end
+
+  def active_bookings_count
+    active_count = current_user.bookings.active.count
+    render json: { active_bookings_count: active_count }
   end
 end
