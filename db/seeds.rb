f82ddc0367619c36b0d8cbc9a36c89a89ff0fce1
porto <pedrolportow@gmail.com>
2024-05-21 18:52:51 -0300

    updated seeds to load initial data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,6 @@
+require 'dotenv'
+Dotenv.load
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -6,25 +9,34 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-owner = User.create(email: "owner@gmail.com", password: "123456", role: "owner", credits: 50, name: "Pedro Porto")
-admin = User.create(email: "admin@gmail.com", password: "123456", role: "admin", credits: 50, name: "Maurício Farias")
-admin = User.create(email: "admin@gmail.com", password: "123456", role: "admin", credits: 50, name: "Marcio")
-user = User.create(email: "user@gmail.com", password: "123456", role: "user", credits: 50, name:"Deisi Moura Rodrigues")
+# owner = User.create(email: "owner@gmail.com", password: "123456", role: "owner", credits: 50, name: "Pedro Porto")
+# admin = User.create(email: "admin@gmail.com", password: "123456", role: "admin", credits: 50, name: "Maurício Farias")
+# admin = User.create(email: "admin@gmail.com", password: "123456", role: "admin", credits: 50, name: "Marcio")
+# user = User.create(email: "user@gmail.com", password: "123456", role: "user", credits: 50, name:"Deisi Moura Rodrigues")
 
-clinic1 = Clinic.create(name: "Casa 1", address: "endereco legal")
-clinic2 = Clinic.create(name: "Casa 2", address: "endereco legal 2")
+byebug
+default_user = User.create(name: ENV['DEFAULT_USER_NAME'], email: ENV['DEFAULT_USER_EMAIL'], password: ENV['DEFAULT_USER_PASSWORD'], credits: 999, role: "owner")
 
-room1 = Room.create(name: "Sala Azul", clinic: clinic1)
-room2 = Room.create(name: "Sala Rosa", clinic: clinic1)
-room3 = Room.create(name: "Sala 42", clinic: clinic2)
-room4 = Room.create(name: "Sala 3", clinic: clinic2)
+clinic1 = Clinic.create(name: "Casa 1", address: "R. Voluntários da Pátria, 684")
+clinic2 = Clinic.create(name: "Casa 2", address: "R. Tiradentes, 2306")
 
-start_date = Time.zone.tomorrow
+Room.create(name: "Sala Azul", clinic: clinic1)
+Room.create(name: "Sala Rosa", clinic: clinic1)
+Room.create(name: "Sala Verde", clinic: clinic1)
+yellow_room = Room.create(name: "Sala Amarela", clinic: clinic1) # Has fixed booking
 
-Booking.create(name: "Reserva ANTIGA (criado com data anterior)", room: room1, user: owner, start_time: start_date - 1.day)
+Room.create(name: "Sala 1", clinic: clinic2)
+Room.create(name: "Sala 2", clinic: clinic2)
+Room.create(name: "Sala 3", clinic: clinic2)
+Room.create(name: "Sala 4", clinic: clinic2)
+Room.create(name: "Sala 5", clinic: clinic2)
 
-Booking.create(name: "Reserva Pedro", room: room1, user: owner, start_time: start_date)
-Booking.create(name: "Reserva teste 2", room: room2, user: owner, start_time: start_date + 1.day)
-Booking.create(name: "Reserva teste 3", room: room3, user: owner, start_time: start_date + 2.day)
-Booking.create(name: "Reserva teste 4", room: room1, user: owner, start_time: start_date + 3.day)
-Booking.create(name: "Reserva teste 5", room: room2, user: owner, start_time: start_date + 5.day)
+# start_date = Time.zone.tomorrow
+
+# Booking.create(name: "Reserva ANTIGA (criado com data anterior)", room: room1, user: owner, start_time: start_date - 1.day)
+
+# Booking.create(name: "Reserva Pedro", room: room1, user: owner, start_time: start_date)
+# Booking.create(name: "Reserva teste 2", room: room2, user: owner, start_time: start_date + 1.day)
+# Booking.create(name: "Reserva teste 3", room: room3, user: owner, start_time: start_date + 2.day)
+# Booking.create(name: "Reserva teste 4", room: room1, user: owner, start_time: start_date + 3.day)
+# Booking.create(name: "Reserva teste 5", room: room2, user: owner, start_time: start_date + 5.day)
