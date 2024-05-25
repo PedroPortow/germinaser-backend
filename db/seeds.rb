36053cacd8520f99a2a1193df7d865a43d98c3b7
porto <pedrolportow@gmail.com>
2024-05-25 09:57:33 -0300

    fix: removed .env from production

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,7 @@
-require 'dotenv'
-Dotenv.load
+if Rails.env.development? || Rails.env.test?
+  require 'dotenv'
+  Dotenv.load
+end
 
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
